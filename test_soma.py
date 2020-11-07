96d7eb13151883d1ee757e0a7ba6b28ce3ec6e3f
Pedro Emidio <p_emidio@outlook.com>
2020-11-07 12:34:13 -0300

    adicionei um arquivo de test para a funcao

---
+++ test_soma.py
@@ -0,0 +1,29 @@
+import unittest
+from .soma import soma
+
+
+class TestSoma(unittest.TestCase):
+    def test_varias_entradas(self):
+        x_y_saidas = (
+            (10, 15, 25),
+            (-10, 10, 0),
+            (1.5, 1.5, 3),
+            (-10, 15.5, 5.5)
+        )
+        for x_y_saida in x_y_saidas:
+            with self.subTest(x_y_saida=x_y_saida):
+                x, y, saida = x_y_saida
+                self.assertEqual(soma(x, y), saida)
+
+    def test_verifica_se_x_e_int_ou_float_retorna_assertionerror(self):
+        with self.assertRaises(AssertionError):
+            soma('10', 5)
+
+
+    def test_verifica_se_y_e_int_ou_float_retorna_assertionerror(self):
+        with self.assertRaises(AssertionError):
+            soma(10, '5')
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=True)
